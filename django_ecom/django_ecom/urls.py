0b8b0fe2033d56e407b06e4d5eeb0d5b1c9598e6
jaswanthinvally <jaswanthvenkat001@gmail.com>
2024-01-25 18:14:20 +0530

    cart route created

--- django_ecom/django_ecom/urls.py
+++ django_ecom/django_ecom/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from main.views import home
+from main.views import cart
 
 urlpatterns = [
     path('', home, name='home'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('cart/', cart, name='cart'),
 ]
